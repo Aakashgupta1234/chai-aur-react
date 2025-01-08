c194e14fdd51be954490f1bad26a850473c9be6d
Aakashgupta@1234 <aakashgupta200207@gmailgit config --global user.name Your>
2025-01-08 20:37:06 +0530

    basic config

---
+++ 03tailwindprops/src/components/Card.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Card(userName){
+    console.log(userName)
+    return(
+        <h1 className='bg-green-400 text-black p-4 rounded-xl'>Tailwind test</h1>
+    )
+}
+export default Card
