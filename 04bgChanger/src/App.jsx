ba2872aeecb18e8e66889b7dffd33cdb3dd68d79
Aakashgupta@1234 <aakashgupta200207@gmailgit config --global user.name Your>
2025-01-10 12:46:27 +0530

    react project basic

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -10,11 +10,12 @@ function App() {
      >
       <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
         <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
-          <button
-          onClick={()=>setColor("red")}
+        <button
+            onClick={()=>setColor("red")}
           className="outline-none px-4 px-4 px-1 rounded-full text-white shadow-lg"
           style={{backgroundColor:"red"}}
-          >Red</button>
+          >red</button>
+           
            <button
             onClick={()=>setColor("green")}
           className="outline-none px-4 px-4 px-1 rounded-full text-white shadow-lg"
@@ -35,5 +36,4 @@ function App() {
      </div>
   )
 }
-
 export default App
